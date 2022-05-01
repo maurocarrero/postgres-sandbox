5e50460f71258a3998d13a4ce4bb2d13a2431451
Carrero, Mauro <mauro.carrero.ctr@sabre.com>
2022-05-01 15:06:53 -0300

    add docker compose
    add env values

---
+++ tapera-mdl.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (name, age, street, city, state, zip, lng, lat) VALUES
+    ('Mauro Carrero', 45, 'Mar de Ajo MG9 S10', 'Ciudad de la Cosa', 'Canelones', 15800, -34.832337, -55.971557)
---
+++ tapera.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+	name	varchar(100),
+	age		integer NOT NULL,
+	street	varchar(200),
+	city	varchar(50),
+	state	varchar(50),
+	zip		integer,
+	lng		double,
+	lat		double
+)
